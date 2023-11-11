f673fe9699ac2d433566a80167ad8e9d1f8eca22
Moad Hamousti <118165767+moadhamousti@users.noreply.github.com>
2023-11-11 17:20:47 +0100

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,37 @@
+const slides = document.querySelector('.slider .slides');
+const images = slides.querySelectorAll('img');
+
+let currentSlide = 0;
+
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+
+leftArrow.addEventListener('click', prevSlide);
+rightArrow.addEventListener('click', nextSlide);
+
+function prevSlide() {
+  images[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide - 1 + images.length) % images.length;
+  images[currentSlide].classList.add('active');
+  scrollCarousel();
+}
+
+function nextSlide() {
+  images[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % images.length;
+  images[currentSlide].classList.add('active');
+  scrollCarousel();
+}
+
+function scrollCarousel() {
+  const slideWidth = images[currentSlide].getBoundingClientRect().width;
+  const slideMargin = parseInt(getComputedStyle(images[currentSlide]).marginLeft);
+  const offset = (slideWidth + slideMargin) * currentSlide;
+  slides.scrollTo({
+    left: offset,
+    behavior: 'smooth'
+  });
+}
+
+images[currentSlide].classList.add('active');
+scrollCarousel();
